d5ef5f98971480aa27f2e48515df9c20f5a3a096
Kerim Moral <moralkerim@gmail.com>
2025-08-19 18:52:18 +0300

    seperate classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <random>
 #include <memory>
+#include "robot.hpp"
 #include "ekf.hpp"
 
-const int SAMPLE_NUMBER = 50;
 
 float sigma_d = 0.01;
 float sigma_a = 0.001;
 
 float q_x = 0.5;
 float q_y = 0.5;
-float q_t = 0.084;
+float q_t = 0.034;
 
 
 Eigen::Matrix2f R({{sigma_d, 0.0f}, {0.0f, sigma_a}}); //Measurement Noise
@@ -28,86 +27,6 @@ auto logger = std::make_shared<Logger>("poses.txt");
 
 EKF ekf(R, Q, P0, X0,logger);
 
-class Robot {
-public:
-    Position pos;
-
-    std::default_random_engine gen;
-    std::normal_distribution<double> dist_noise;
-    std::normal_distribution<double> angle_noise;
-
-    std::normal_distribution<double> range_noise;
-    std::normal_distribution<double> bear_noise;
-
-    Robot(double x=0, double y=0, double theta=0, std::shared_ptr<Logger> logger_ = nullptr)
-    : pos(x, y, theta),
-      dist_noise(0.0,  q_x),     // 0.1m std dev
-      angle_noise(0.0, q_t),   // 1 degree ≈ 0.017 rad
-      range_noise(0.0, sigma_d),     // 0.1m std dev
-      bear_noise(0.0, sigma_a),   // 1 degree ≈ 0.017 rad
-      logger(logger_)
-    {}
-
-    void move(double distance, double angle) {
-        // Gürültü ekle
-        Position pos_sample;
-        for(int i=0;i<SAMPLE_NUMBER-1;i++) {
-            double noisy_distance = distance + dist_noise(gen);
-            double noisy_angle = angle + angle_noise(gen);
-
-            pos_sample.theta = pos.theta + noisy_angle;
-            pos_sample.x = pos.x + noisy_distance * cos(pos_sample.theta);
-            pos_sample.y = pos.y + noisy_distance * sin(pos_sample.theta);
-            logger->logPosition("Gaussian",pos_sample);
-        }
-
-            pos.theta = pos_sample.theta;
-            pos.x = pos_sample.x;
-            pos.y = pos_sample.y;
-            logger->logPosition("Actual",pos);
-    }
-
-
-
-    void print() const {
-        std::cout << "Robot position: x=" << pos.x << ", y=" << pos.y << ", theta=" << pos.theta << std::endl;
-    }
-
-    std::vector<Measurement> senseLandmarks(const std::unordered_map<int, std::shared_ptr<Landmark>> & landmarks,
-                            double range_limit = 10.0,
-                            double bearing_limit_deg = 90.0)
-    {
-        double bearing_limit_rad = bearing_limit_deg * M_PI / 180.0;
-        std::vector<Measurement> measurements;
-
-        std::cout << "  Sensor readings:" << std::endl;
-        for (const auto& [id, lm] : landmarks) {  
-            double dx = lm->x - pos.x;
-            double dy = lm->y - pos.y;
-
-            double range = std::sqrt(dx * dx + dy * dy) + range_noise(gen);
-            double bearing = std::atan2(dy, dx) - pos.theta + bear_noise(gen);
-
-
-            // Normalize bearing to [-pi, pi]
-            if (bearing > M_PI) bearing -= 2 * M_PI;
-            if (bearing < -M_PI) bearing += 2 * M_PI;
-
-            //Check if we have a measurement
-            if (range <= range_limit && std::abs(bearing) <= bearing_limit_rad) {
-                std::cout << "    Landmark " << id << ": range=" << range 
-                        << ", bearing=" << bearing << std::endl;
-                Measurement z(id, range, bearing); 
-                measurements.emplace_back(z);
-            }
-        }
-        return measurements;
-    }
-    
-private:
-    std::shared_ptr<Logger> logger;
-
-};
 
 std::shared_ptr<Landmark> FindAssociation(
     const Measurement& z,
